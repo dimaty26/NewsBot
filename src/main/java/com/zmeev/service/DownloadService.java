05ec87e3a56133cd63ff610873699e4893dba94f
dimaty26 <dimaty26rus@mail.ru>
2024-10-07 20:05:49 +0200

    Initial commit

---
+++ src/main/java/com/zmeev/service/DownloadService.java
@@ -0,0 +1,8 @@
+package com.zmeev.service;
+
+/**
+ * This service should load news from different platforms
+ * from here https://www.berlin.de/polizei/polizeimeldungen/ as example
+ */
+public class DownloadService {
+}
---
+++ src/main/java/com/zmeev/service/TelegramService.java
@@ -0,0 +1,4 @@
+package com.zmeev.service;
+
+public class TelegramService {
+}
